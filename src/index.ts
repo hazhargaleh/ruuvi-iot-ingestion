969913fc2a0409ef0eb7b7f08c004eeddd5d5ba7
Hazhar Galeh <hazhar.galeh@ecodev.ch>
2026-03-17 19:35:32 +0100

    Remove MAC name mappings logging from main function

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,8 @@
 import { startHttpServer } from "./http/healthServer.js";
 import { startMqtt } from "./mqtt/mqttService.js";
 import { logger } from "./logger/logger.js";
-import { config } from "./config/env.js";
 async function main() {
   await startHttpServer();
-  logger.info(
-    {
-      gateways: config.gatewayNames,
-      tags: config.tagNames,
-    },
-    'Loaded MAC name mappings',
-  );
   startMqtt();
   logger.info("Ruuvi ingestion service started");
 }
